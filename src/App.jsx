00e00c23ab20b65149c47ee69579a33c6263f268
LunaticStruggler <outdoor.co.se@gmail.com>
2024-11-11 11:19:15 +0100

    Fixed import case for Header component and other minor adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
-import Header from './components/header';
+import Header from './components/Header';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
